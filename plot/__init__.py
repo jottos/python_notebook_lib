8e19b0d55c80260863b69f3d12479b98cb0e9950
jottos <jottos+github@gmail.com>
2019-12-05 19:10:09 -0800

    fixed issue with plotting by colors into a subplot, added more options

--- plot/__init__.py
+++ plot/__init__.py
@@ -8,7 +8,7 @@ import joslib.stats
 __version__ = "0.1.0"
 __all__ = ['is_outlier', 'regression_plot', 'plot_groups_with_different_colors', 'plot_percentiles']
 
-def plot_percentiles(df:pd.DataFrame, columns, how_many=10, logy=False) -> None:
+def plot_percentiles(df:pd.DataFrame, columns, how_many=10, logy=False, figsize:Tuple[float]=(15.0,15.0)) -> None:
     '''
     break dataframe into how_many n'ciles and plot the specified columns as x,y
     scatter. 
@@ -23,17 +23,66 @@ def plot_percentiles(df:pd.DataFrame, columns, how_many=10, logy=False) -> None:
     def make_percentiles(how_many):
         return [n/how_many for n in range(how_many)]
     x = df[columns].describe(percentiles=make_percentiles(how_many))
-    x[4:].plot(logy=logy)
+    x[4:].plot(logy=logy, figsize=figsize)
 
 
 def plot_groups_with_different_colors(
         grouped_df:pd.core.groupby.generic.DataFrameGroupBy,
         x_name:str, 
         y_name:str,
-        figsize:Tuple[float]=(25.0,25.0), 
+        figsize:Tuple[float]=(10.0,10.0),
+        ax=None,
         legend:bool=True, 
         logplot:bool=False,
-        max_groups:int=None) -> None:
+        max_groups:int=None,
+        title=None,
+        marker='.') -> None:
+    """
+    given a DataFrameGroupBy scatter plot the content coloring each group differently
+    
+    x_name - column name for x dimension
+    y_name - column name for y dimension
+    
+    figsize - set size of plot, default is pretty big
+    legend  - show legend if true
+    logplot - plot in log space if true
+    max_groups - show ownly this many groups, default is all
+    marker  - change the point representation, default is "." == "point", "o" = circle "," = pixel
+    
+
+    NOTE: there are only 10 colors so more than 10 groups is no bueno
+    NOTE: no null/nan values allowed in the x or y columns
+
+    TODO: make sure types check out in param list, the implied optionals might hurt
+    """
+    if not ax:
+        fig, ax = plt.subplots(figsize=figsize)
+    if title:
+        ax.set_title(title)
+    ax.margins(0.05) # Optional, just adds 5% padding to the autoscaling
+    if logplot:
+        ax.set_yscale('log')
+        ax.set_xscale('log')
+    group_count = 0
+    for name, group in grouped_df:
+        ax.plot(group[x_name], group[y_name], marker=marker, linestyle='', ms=12, label=name)
+        group_count += 1
+        if max_groups and group_count == max_groups:
+            break
+    if legend:
+        ax.legend()
+
+        
+def plot_groups_with_different_colors(
+        grouped_df:pd.core.groupby.generic.DataFrameGroupBy,
+        x_name:str, 
+        y_name:str,
+        figsize:Tuple[float]=(10.0,10.0),
+        ax=None,
+        legend:bool=True, 
+        logplot:bool=False,
+        max_groups:int=None,
+        title=None) -> None:
     """
     given a DataFrameGroupBy scatter plot the content coloring each group differently
     
@@ -50,24 +99,27 @@ def plot_groups_with_different_colors(
 
     TODO: make sure types check out in param list, the implied optionals might hurt
     """
-    fig, ax = plt.subplots(figsize=figsize)
+    if not ax:
+        fig, ax = plt.subplots(figsize=figsize)
     ax.margins(0.05) # Optional, just adds 5% padding to the autoscaling
     if logplot:
         ax.set_yscale('log')
         ax.set_xscale('log')
     group_count = 0
     for name, group in grouped_df:
-        ax.plot(group[x_name], group[y_name], marker='o', linestyle='', ms=12, label=name)
+        #ax.plot(group[x_name], group[y_name], marker='o', linestyle='', ms=12, label=name)
+        ax.plot(group[x_name], group[y_name], marker='.', linestyle='', ms=12, label=name)
         group_count += 1
         if max_groups and group_count == max_groups:
             break
     if legend:
         ax.legend()
-    plt.show()
 
 
-
-def regression_plot(data_frame:pd.DataFrame, x_column_name:str, y_column_name:str) -> None:
+def regression_plot(data_frame:pd.DataFrame,
+                    x_column_name:str,
+                    y_column_name:str,
+                    figsize:Tuple[float]=(20.0,10.0)) -> None:
     """
     given a data frame and two columns to use, create a scatter diagram and regression line
     do this in normal and log(10) space and plot
@@ -121,7 +173,7 @@ def regression_plot(data_frame:pd.DataFrame, x_column_name:str, y_column_name:st
 
 
     # Plot stuff, setup to do supplots
-    fig, axes = plt.subplots(nrows=1, ncols=2)
+    fig, axes = plt.subplots(figsize=figsize, nrows=1, ncols=2)
 
     # Plot the original data and model
     data.plot(kind='scatter', color='Blue', x=x_column_name, y=y_column_name, ax=axes[0], title='Normal Space')
